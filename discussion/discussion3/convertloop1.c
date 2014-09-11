4970519516c7d57c45281aaffd392a4fb920b8d6
dev-seahouse <xinkenan.11@hotmail.com>
2014-09-11 15:43:01 +0800

    added new files

---
+++ discussion/discussion3/convertloop1.c
@@ -0,0 +1,18 @@
+
+sum = 0;
+i = -5;
+do {
+    sum += i;
+    i += 5;
+} while (i < 100);
+
+printf("sum = %d\n", sum);
+
+i=-5;
+while (i<100){
+	sum +=i;
+	i+=5;
+
+}
+printf("sum = %d\n", sum);
+
