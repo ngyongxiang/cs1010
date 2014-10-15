f9e42d305a60336dc7c12706d558d37dc08d4831
dev-seahouse <xinkenan.11@hotmail.com>
2014-10-15 16:14:30 +0100

    new

--- lab/lab4/pyramid.c
+++ lab/lab4/pyramid.c
@@ -1,11 +1,24 @@
 #include <stdio.h>
+#define MAX_SIZE 10
 
-int scanTriangularArray (int *, int);
-void printTriangularArray(int *);
-void 
+
+int scanTriangularArray(int *);
+
 
 int main(void){
+	int size,i;
+	int pyramid[][MAX_SIZE];
+
+	// Read a possitive integer value for size.
+	// size <= 10
+	printf("Enter number of rows: ");
+  	scanf("%d", size);
+
+	// Create a 2 dimensional array whereby first row has 1 element, second row 2 element etc.
+	// * No of rows = no. of elements, max elements 10
 	
+	
+		
 
 	return 0;
 }
