18df0117e54c64188890a539835bef388b6b664f
dev-seahouse <xinkenan.11@hotmail.com>
2015-01-30 11:15:35 +0800

    liftsimultion updated

--- lab/lab1/liftSimulation_task1.c
+++ lab/lab1/liftSimulation_task1.c
@@ -3,34 +3,36 @@
 /*add necessary headfiles here*/
 
 //************************  TASK 1 **********************************//
+// This function places a > on the 8th position in the array based on cur_pos value 
 char ** staticLift(int cur_pos)
 {
+	
 	/*add your code here*/
-	return  0;
-}
-//************************  TASK 1 **********************************//
-int print_arr(char output[][12]);
-//*************************  ENTRY *********************************//
-int main()
-{
-	char output[ ][12] = 
+		char output[ ][12] = 
 	    {{'-','-','-','-','-','-','-','-','-','-','-','-'},
 		{'|','-','-','-','-','-','-','[','_','_',']','|'},
 		{'|','-','-','-','-','-','-','[','_','_',']','|'},
 		{'|','-','-','-','-','-','-','[','_','_',']','|'},
 		{'|','-','-','-','-','-','-','[','_','_',']','|'},
 		{'|','-','-','-','-','-','-','[','_','_',']','|'}};
-
+		
+		// replace character based on cur_pos
+		output[cur_pos][12] = '>';
+			
+	return output;
+}
+//************************  TASK 1 **********************************//
+int print_arr(char output[][12]);
+//*************************  ENTRY *********************************//
+int main()
+{
 	/*Task 1*/
 	int cur_pos;
-	//scanf("%d",&cur_pos);
+	scanf("%d",&cur_pos);
 
 	char **array1=staticLift(cur_pos);
-	print_arr(output);
+
 		
-
-
-
 	return 0;
 }
 
@@ -42,8 +44,7 @@ int print_arr(char output[][12]){
 
 	for (i = 0; i < row; i ++){
 		for (j = 0; j < col; j++){
-			printf("%c",output[i][j]);
-			
+			putchar(output[i][j]);	
 		}
 		printf("\n");
 	}
