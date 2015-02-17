f584abbe5a4591856861d45b575f68a643fa0f1f
Kenan <xinkenan.11@hotmail.com>
2015-02-17 12:53:18 +0800

    rand

--- unitdemo/New Folder/Week6_SumToRandomPos.c
+++ unitdemo/New Folder/Week6_SumToRandomPos.c
@@ -20,6 +20,10 @@ int main(void) {
 
 	// call sumPartial() function below
 
+	srand(time(NULL));
+	upToRow = rand()%row;
+	upToRow = rand()%col;
+
 	printf("Sum to position [%d][%d] = %.2f\n"); // incomplete 
 
 	return 0;
@@ -33,6 +37,22 @@ void scanArray(float arr[][MAX_COLS], int rows, int cols) {
 	for (r=0; r < rows; r++)
 		for (c=0; c < cols; c++)
 			scanf("%f", &arr[r][c]);
+}
+
+float sumPartial(float arr[][MAX_COLS],int rows,int cols, int upToRow, int upToCol){
+
+	float sum = 0 ;
+	int r,c;
+	for (r = 0; i < upToRow; r++){
+		for (c = 0 ; c < cols ; c ++)
+			sum+=arr[r][c];
+	}
+
+	for (c = 0; c < upToCol; c++){
+		sum += arr[upToRow][c];
+	}
+
+	return sum;
 }
 
 // Sum elements from position [0][0] to a random 
